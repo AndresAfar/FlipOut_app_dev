fb548d973d845af4f32159296247dfa858b44100
AndresAfar <afar2503@gmail.com>
2024-07-20 14:46:01 -0500

    Se modifican stylos

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -44,22 +44,22 @@ export const Login = () => {
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <img className="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Iniciar Sesión</h2>
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-300">Iniciar Sesión</h2>
       </div>
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <form className="space-y-6" onSubmit={handleSubmit}>
           <div>
-            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Correo Electrónico:</label>
+            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-300">Correo Electrónico:</label>
             <input id="email" name="email" type="email" value={credentials.email} onChange={handleChange} required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
           <div>
-            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Contraseña:</label>
+            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-300">Contraseña:</label>
             <input id="password" name="password" type="password" value={credentials.password} onChange={handleChange} required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
           <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Iniciar Sesión</button>
           {error && <p>{error}</p>}
         </form>
-        <p className="mt-10 text-center text-sm text-gray-500">¿No tiene cuenta? <a href="/register" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"> Registrarse</a></p>
+        <p className="mt-10 text-center text-sm text-gray-400">¿No tiene cuenta? <a href="/register" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"> Registrarse</a></p>
       </div>
     </div>
   );
